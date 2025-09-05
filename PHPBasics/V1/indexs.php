6399bf91374f27a112f5477279fc266ffb8b4c84
Kite.jarvis <Mightykitey1@gmail.com>
2025-09-05 13:13:20 +0100

    start a new PHP webpage

--- PHPBasics/V1/indexs.php
+++ PHPBasics/V1/indexs.php
@@ -1,15 +1,58 @@
 <?php //pushes everything throw the interceptor
 
+echo "<!DOCTYPE html>";
+
+// declaration for it
+
 echo "<html>";
 
-echo "<head>";
-echo "<title>Kyte</title>";
-echo "</head>";
+    echo "<head>";
 
-echo "<body>";
-echo "<h1>Kyte</h1>";
-echo "This is my site";
-echo"</body>";
+        echo "<title>Kyte</title>";
+        echo "<img = src='cheering.jfif' alt='Kyte'>"; // there is my image
+
+    echo "</head>";
+
+    // This is not an ideal way but due to IT  system being unstable
+
+    echo "<body>";
+
+        echo "<h1>Kyte</h1>";
+
+        echo "This is my page";
+
+        echo "<p>This is a paragraph</p>";
+
+        echo "<hr>"; // this just make a line
+        echo "<p>Em vs i </p>";
+
+        echo "<em>What does this do?</em>";
+        echo "<br>";
+        echo "<i>What does this do?</i>"; // make it all squiggly
+
+        echo "<hr>";
+        echo "<Strong>Im best</Strong>"; // bolds
+        echo "<br>"; // it breaks the line to a different line
+        echo "<b>No im the best</b>"; // bolds
+        echo "<br>";
+        echo "<u>Im not underlined</u>"; // underlines text
+        echo "<br>";
+
+        echo "<hr>";
+        echo "<a href='\page2.php'> go to page 2</a> ";
+
+        echo "<table>";
+
+        echo "<tr>";
+
+        echo "<th>hello1</th>";
+        echo "<th>hello2</th>";
+        echo "<th>hello3</th>";
+
+        echo"</tr>";
+        echo"</table>";
+
+    echo "</body>";
+
 
 echo "</html>";
-?>
