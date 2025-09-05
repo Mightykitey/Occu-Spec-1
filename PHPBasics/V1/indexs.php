33f35c4fbd493810b688685d592bfbf7c029ad14
Kite.jarvis <Mightykitey1@gmail.com>
2025-09-05 10:37:31 +0100

    start a new PHP webpage

---
+++ PHPBasics/V1/indexs.php
@@ -0,0 +1,15 @@
+<?php //pushes everything throw the interceptor
+
+echo "<html>";
+
+echo "<head>";
+echo "<title>Kyte</title>";
+echo "</head>";
+
+echo "<body>";
+echo "<h1>Kyte</h1>";
+echo "This is my site";
+echo"</body>";
+
+echo "</html>";
+?>
