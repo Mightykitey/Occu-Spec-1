f4a5b4221b8c800eb87b1bbedcdd35b31a07eea6
Kite.jarvis <Mightykitey1@gmail.com>
2025-09-09 10:29:02 +0100

    wip

--- PHPBasics/mini project 01/index.php
+++ PHPBasics/mini project 01/index.php
@@ -7,7 +7,7 @@ echo"<link rel='stylesheet' href='css/styles.css'>";
 echo"</head>";
 echo"<body>";
     echo"<h1>Black holes</h1>";
-    echo"<p>test</p>";
+    echo"<p>Wip</p>";
 
 echo"</body>";
 echo"</html>";
