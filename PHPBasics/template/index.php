88c19543b9061b49cc37ff04a5c2b05fe9eabe01
Kite.jarvis <Mightykitey1@gmail.com>
2025-09-15 12:11:05 +0100

    wip

---
+++ PHPBasics/template/index.php
@@ -0,0 +1,29 @@
+<?php
+
+echo"<!doctype html>";
+
+echo"<html>";
+
+echo"<head>";
+
+    echo"<title></title>";
+echo"<link rel='css/styles.css' rel='stylesheet'>";
+
+echo"</head>";
+
+echo"<body>";
+
+    echo"<h1></h1>";
+    echo"<p></p>";
+
+    echo"<table>";
+
+        echo"<tr>";
+        echo"<td></td>>";
+
+    echo"</table>";
+echo"</body>";
+
+echo"</html>";
+
+?>
