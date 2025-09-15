9fbc6e9f4547937beed077501bc65f5cb00be9f1
Kite.jarvis <Mightykitey1@gmail.com>
2025-09-15 12:22:31 +0100

    push

---
+++ PHPBasics/mini-project02/Solutions/assest/nav.php
@@ -0,0 +1,19 @@
+<?php
+
+echo"<div class='navi'>";
+
+echo"<nav>";
+
+    echo"<ul>";
+            //<!--my element that transport the user to different part of the website-->
+        echo"<li><a href='classes.php'>claseses</a></li>";
+        echo "<li><a href='booking.php'>Booking</a></li>";
+        echo"<li><a href='sign.php'>Sign in</a></li>";
+        echo"<li><a href='reviews.'>Reviews</a></li>";
+        echo "<li><a href='index.php'>Home</a></li>";
+
+    echo"</ul>";
+
+echo"</nav>";
+
+echo"</div>";
--- PHPBasics/mini-project02/Solutions/index.php
+++ PHPBasics/mini-project02/Solutions/index.php
@@ -8,10 +8,13 @@ echo"</head>";
     echo"<body>";
         echo "<img id='logo' src='images/logo.jpg'  alt='logo' >";
         echo"<h1>GibJohn Tutoring</h1>";
-        echo"<table>";
-        echo"<tr><td><a href='sign.php'>Sign in</a></td></tr>";
-        echo"<tr><td><a href='reviews.php'>Reviews</a></td></tr>";
-        echo"<tr><td><a href='booking.php'>Booking</a></td></td>";
-        echo"<tr><td><a href='classes.php'>Classes</a></td></tr>";
-        echo"</table>";
+
+echo "<div class='container'>";
+
+        require_once  'assest/nav.php';
+
+
+
     echo"<img id='online' src='images/online.jpg' alt='onlineprogress'>";
+    echo"</body>";
+    echo"</html>";
