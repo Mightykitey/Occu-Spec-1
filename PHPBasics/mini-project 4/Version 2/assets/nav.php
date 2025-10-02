60bc08050f800878f0c2fa37ecb09ac814edd35a
Milvus Milvus <138457184+Mightykitey@users.noreply.github.com>
2025-10-02 08:49:48 +0100

    Add files via upload

---
+++ PHPBasics/mini-project 4/Version 2/assets/nav.php
@@ -0,0 +1,19 @@
+<?php
+
+echo"<div class='navi'>";
+
+echo"<nav>";
+
+    echo"<ul>";
+            //<!--my element that transport the user to different part of the website-->
+        echo"<li><a href="."></a></li>";
+        echo "<li><a href="."></a></li>";
+        echo"<li><a href="."></a></li>";
+        echo"<li><a href="."></a></li>";
+        echo "<li><a href="."></a></li>";
+
+    echo"</ul>";
+
+echo"</nav>";
+
+echo"</div>";
---
+++ PHPBasics/mini-project 4/Version 3/index.php
@@ -0,0 +1,38 @@
+<?php
+session_start();/*each page not file*/
+
+require_once "assets/commonfunk.php"; //require_once only happen once anywhere
+
+if ($_SERVER["REQUEST_METHOD"] === 'POST') { // super global veurable
+    // echo $_POST["message"];
+    $_SESSION['msg'] = $_POST["message"];
+
+}
+
+echo"<!doctype html>";
+
+echo"<html>";
+
+echo"<head>";
+
+    echo"<title></title>";
+echo"<link rel='css/styles.css' rel='stylesheet'>";
+
+echo"</head>";
+
+echo"<body>";
+
+
+echo "<div class='content'>";
+
+echo usr_msg();
+
+echo "<form method='post' action=''>";
+
+
+
+echo"</body>";
+
+echo"</html>";
+
+?>
